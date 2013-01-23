b98fe7e2c719874ed45e5150f639f272cd6dec1d
Kevin Ho <keho98@gmail.com>
2013-01-23 00:21:25 -0800

    Adding specs

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe ImagesController do
-  describe 'searching TMDb' do
-    it 'should call the model method that performs TMDb search' do
+  describe 'show images' do
+    it 'should get all images needed' do
 
     end
   end
