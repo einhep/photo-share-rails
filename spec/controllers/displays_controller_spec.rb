33547fd95513f3253b8e26b0a93d51da63422b36
Kevin Ho <keho98@gmail.com>
2013-01-26 20:01:37 -0800

    Changing spec

--- spec/controllers/displays_controller_spec.rb
+++ spec/controllers/displays_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe DisplaysController do
-  describe 'getting Images' do
-    it 'should get all images in the database' do
+  describe 'GET images' do
+  	it 'gets the associated images for the display' do
 
-    end
+  	end
   end
 end
